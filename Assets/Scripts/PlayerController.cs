0f9714590de98b2f35e8f2425305cedc4f2ad342
mihakorakotoson-ctrl <mihakorakotoson@gmail.com>
2026-09-04 20:24:30 +0300

    Add PlayerController with movement and property buying

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,1 +1,24 @@
+using UnityEngine;
+using System.Collections.Generic;
 
+public class PlayerController : MonoBehaviour
+{
+    public string playerName = "Rakotoson";
+    public int money = 150000; // Ariary
+    public int position = 0;
+    public List<string> properties = new List<string>();
+
+    public void Move(int steps, int boardSize)
+    {
+        position = (position + steps) % boardSize;
+    }
+
+    public void BuyProperty(string property, int cost)
+    {
+        if (money >= cost)
+        {
+            money -= cost;
+            properties.Add(property);
+        }
+    }
+}
