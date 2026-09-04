d59af187d0db9766da51bce07b56ad73356ae5c7
mihakorakotoson-ctrl <mihakorakotoson@gmail.com>
2026-09-04 20:23:55 +0300

    Update DiceManager.cs

--- Assets/Scripts/DiceManager.cs
+++ Assets/Scripts/DiceManager.cs
@@ -1,1 +1,11 @@
+using UnityEngine;
 
+public class DiceManager : MonoBehaviour
+{
+    public int RollDice()
+    {
+        int die1 = Random.Range(1, 7);
+        int die2 = Random.Range(1, 7);
+        return die1 + die2;
+    }
+}
