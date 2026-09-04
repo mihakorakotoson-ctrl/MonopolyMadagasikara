f9bba4615185d967bea5013e8b55c38d1fcee8a6
mihakorakotoson-ctrl <mihakorakotoson@gmail.com>
2026-09-04 20:25:14 +0300

    Add board spaces to BoardManager class

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,1 +1,20 @@
+using UnityEngine;
+using System.Collections.Generic;
 
+public class BoardManager : MonoBehaviour
+{
+    public List<string> boardSpaces = new List<string>()
+    {
+        "Avenue de l’Indépendance",
+        "Hotel Colbert",
+        "Rova d’Ambohimanga",
+        "Nosy Be",
+        "Île Sainte Marie",
+        "Parc National d’Andasibe",
+        "Baobab de Morondava",
+        "Fort Dauphin",
+        "Isalo National Park",
+        "Université d’Antananarivo",
+        "Parc Zoologique de Tsimbazaza"
+    };
+}
