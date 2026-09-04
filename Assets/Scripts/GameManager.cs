b024e372f195da5d321f6672850054983d70b39e
mihakorakotoson-ctrl <mihakorakotoson@gmail.com>
2026-09-04 20:25:51 +0300

    Implement GameManager for game control and player actions
    Added GameManager class to handle game logic and player movement.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,1 +1,19 @@
+using UnityEngine;
 
+public class GameManager : MonoBehaviour
+{
+    public PlayerController player1;
+    public BoardManager board;
+    public DiceManager dice;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) // touche espace = lancer les dés
+        {
+            int roll = dice.RollDice();
+            player1.Move(roll, board.boardSpaces.Count);
+            Debug.Log(player1.playerName + " avance de " + roll + 
+                      " cases et arrive sur " + board.boardSpaces[player1.position]);
+        }
+    }
+}
